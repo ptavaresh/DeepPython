3d9477cdf1894a074c80800bc8accfd11f4032aa
Pedro Tavares <ptavaresh@gmail.com>
2019-08-17 17:08:47 -0500

    fist commit

---
+++ app/lab/scripts/code_to_test_bandit.py
@@ -0,0 +1,9 @@
+
+#Bandit examples
+password='mypassword123'
+
+name = raw_input("What is your name?")
+cur.execute("SELECT * FROM Students WHERE Name = '%s';" % name)
+
+requests.get('https://snyk.io', verify=False)
+
